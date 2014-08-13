8add63641cef2fb233a10632da25eceb7ad04ed8
David Megginson <david.megginson@megginson.com>
2014-08-13 08:26:46 -0400

    First stub JUnit4 test working.

---
+++ src/test/java/org/hxlstandard/HXLValueTest.java
@@ -0,0 +1,23 @@
+package org.hxlstandard;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit4 tests for the HXLValue class.
+ */
+public class HXLValueTest {
+
+    private HXLValue value;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testNothing() {
+        Assert.assertTrue(true);
+    }
+
+}
